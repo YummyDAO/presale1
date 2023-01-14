884298fca89fd77f787204fefcc95c37bfd44eb2
yummyDAO <rogerscott650@gmail.com>
2023-01-14 01:19:01 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,235 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.png';
+import maddy from './maddy.jpeg'
 import './App.css';
 import Hamburger from './components/Hamburger'
 import SideMenu from './components/SideMenu'
 import Countdown from 'react-countdown'
+import {ethers, utils} from "ethers";
+import contractAbi from './contract.json';
+//import ERC20Abi from './utils/Erc20faucet.json';
+//abi
 
 function App() {
 
+  const CONTRACT_ADDRESS = '0x4175f81c5DE65E16A777769cFC85ADe675778fBb';//vault
+const ERC20 = '0x674de548F003506d5B2001413517e2d1ad00348b';//testtoken
+const YIELD = '0x674de548F003506d5B2001413517e2d1ad00348b';
+
   const [isMenuActive, activeMenu] = useState(false)
-  const getLocalStorageValue  = (s) => localStorage.getItem(s);
-  const [data, setData] = useState(
-    {date: Date.now(), delay: 25200000}
-  );
-  const wantedDelay = 25200000;
+  const [currentAccount, setCurrentAccount] = useState('');
+  const [value, setValue] = useState('');
+  const [tvl1, settvl1] = useState('');
+  const [balwallet, setbalwallet] = useState('');
+
+  const connectWallet = async () => {
+		try {
+			const { ethereum } = window;
+
+			if (!ethereum) {
+				alert("Get MetaMask -> https://metamask.io/");
+				return;
+			}
+
+			// Fancy method to request access to account.
+			const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+		
+			// Boom! This should print out public address once we authorize Metamask.
+			console.log("Connected", accounts[0]);
+			setCurrentAccount(accounts[0]);
+		} catch (error) {
+			console.log(error)
+		}
+	};
+
+  	const checkIfWalletIsConnected = async () => {
+		// First make sure we have access to window.ethereum
+		const { ethereum } = window;
+	
+		if (!ethereum) {
+			console.log("Make sure you have MetaMask!");
+			return;
+		} else {
+			console.log("We have the ethereum object", ethereum);
+		}
+
+		// Check if we're authorized to access the user's wallet
+		const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+		// Users can have multiple authorized accounts, we grab the first one if its there!
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			console.log('Found an authorized account:', account);
+			setCurrentAccount(account);
+		} else {
+			console.log('No authorized account found');
+		}
+	};
+
+  const deposit = async () => {
+	  try {
+		const { ethereum } = window;
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+		if (ethereum) {
+		  const provider = new ethers.providers.Web3Provider(ethereum);
+		  const signer = provider.getSigner();
+		  const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+		  let amount = ethers.utils.parseUnits(value, "ether");
+	
+				console.log("check deposit value.", amount.toString())
+        //console.log(signer.getAddress())
+
+        // Users can have multiple authorized accounts, we grab the first one if its there!
+          const account = accounts[0];
+          console.log(account)
+        //const address = await signer.getAddress();
+		  let tx = await contract.buyTokens(account.toString(), amount.toString(),{value: amount.toString()});
+		  // Wait for the transaction to be mined
+				const receipt = await tx.wait();
+	
+				// Check if the transaction was successfully completed
+				if (receipt.status === 1) {
+					console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);//change to oklink
+					
+					// Set the record for the domain
+					//tx = await contract.setDetails (domain, record);
+					//await tx.wait();
+	
+					console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
+					// Call fetchMints after 2 seconds
+				    //setTimeout(() => {
+					    //fetchMints();
+				    //}, 5000);
+					
+					//setRecord('');
+					//setDomain('');
+				}
+				else {
+					alert("Transaction failed! Please try again");
+				}
+		}
+	  }
+	  catch(error){
+		console.log(error);
+	  }
+	};
+
+  const withdraw = async () => {
+	  try {
+		const { ethereum } = window;
+		if (ethereum) {
+		  const provider = new ethers.providers.Web3Provider(ethereum);
+		  const signer = provider.getSigner();
+		  const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+		  //let amount = ethers.utils.parseUnits(value.toString(), "ether");
+	
+				//console.log("check deposit value.", amount)
+		  let tx = await contract.withdrawTokens();
+		  // Wait for the transaction to be mined
+				const receipt = await tx.wait();
+	
+				// Check if the transaction was successfully completed
+				if (receipt.status === 1) {
+					console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);//change to oklink
+					
+					// Set the record for the domain
+					//tx = await contract.setDetails (domain, record);
+					//await tx.wait();
+	
+					console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
+					// Call fetchMints after 2 seconds
+				    //setTimeout(() => {
+					    //fetchMints();
+				    //}, 5000);
+					
+					//setRecord('');
+					//setDomain('');
+				}
+				else {
+					alert("Transaction failed! Please try again");
+				}
+		}
+	  }
+	  catch(error){
+		console.log(error);
+	  }
+	};
+
+    const fetchbalance = async () => {
+    try {
+    const { ethereum } = window;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+      const address1 = currentAccount;
+      const params = await contract.getBalance();
+      console.log("56",params)
+
+      let tx = utils.formatUnits(params, 18);
+      console.log("12",tx)
+      let newvalue = tx.toString();
+      console.log(newvalue)
+      const ethvalue = ethers.utils.formatEther(newvalue)
+      const ethvalue1 = Math.round(ethvalue * 1e4) / 1e4;
+      console.log("tx",ethvalue)
+      //console.log("Record set https://mumbai.polygonscan.com/tx/"+tx.hash);
+      setbalwallet(Math.round(tx))
+
+      //fetchMints();
+      //setRecord('');
+      //setDomain('');
+    }
+    } catch(error) {
+    console.log(error);
+    }
+  //setLoading(false);
+};
+
+  const fetchRaised = async () => {
+    try {
+    const { ethereum } = window;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+      const address1 = currentAccount;
+      const params = await contract.TotalRaised();
+      console.log("p",params.toString())
+
+      /*let tx = utils.formatUnits(params, 18);
+      console.log("tx",tx)
+      let newvalue = tx.toString();
+      console.log(newvalue)
+      const ethvalue = ethers.utils.formatEther(newvalue)
+      const ethvalue1 = Math.round(ethvalue * 1e4) / 1e4;
+      console.log("value",ethvalue)*/
+      //console.log("Record set https://mumbai.polygonscan.com/tx/"+tx.hash);
+      settvl1(params.toString())
+
+      console.log("tvl",tvl1)
+      console.log("value", value)
+
+      //fetchMints();
+      //setRecord('');
+      //setDomain('');
+    }
+    } catch(error) {
+    console.log(error);
+    }
+  //setLoading(false);
+};
+
+useEffect(() => {
+  checkIfWalletIsConnected();
+}, [])
 
   useEffect(() => {
-    const savedDate = getLocalStorageValue("end_date");
-    if (savedDate != null && !isNaN(savedDate)){
-      const currentTime = Date.now();
-      const delta = parseInt(savedDate, 10) - currentTime;
-
-      if (delta > wantedDelay){
-        if (localStorage.getItem("end_date").length > 0)
-        localStorage.removeItem("end_date");
-      }
-      else {
-        setData({date: currentTime, delay: delta});
-      }
-    }
-  }, []);
+  setTimeout(() => {
+  fetchbalance()
+  fetchRaised()
+  }, 5000)
+}, [fetchbalance,fetchRaised])
   return (
     <div className="App">
       <div className="" id="gatsby-focus-wrapper">
@@ -78,11 +279,38 @@ function App() {
           </div>
         </div>
         <div className='presale'>
-          <h1 className='presale-text'>Presale starts in</h1>
-          <div className='presale-body'>
-            {/*<span>12 : 10 : 10</span>*/}
+          {/*<h1 className='presale-text'>Presale starts at</h1>*/}
+          <div className='presale-body flex'>
+            <div className="presale-card1">
+              <span>Buy Maddy Token</span>
+              <div className='next'>
+                <span className='next-text'>Amount in ETH</span>
+                <input onChange={e => setValue(e.target.value)}></input>
+              </div>
+              <div className='next1'>
+              <span className='next-text'>0.000000206005754427 ether per token</span>
+                <span></span>
+              </div>
+              { currentAccount ?<button className='btn' onClick={deposit}>Buy Now</button> :
+              <button className='btn' onClick={connectWallet }>Connect Wallet</button>}
+            </div>
+            <div className="presale-card2 flex">
+              <div className='maddytab'>
+                <img className='maddyimg' src={maddy} alt='maddy'></img>
+                <span className='wes'>Maddy balance</span>
+                <span>{balwallet}</span>
+                <button className='btn' onClick={withdraw}>Claim</button>
+              </div>
+              <div className='esmaddytab maddytab'>
+              <img className='maddyimg' src={maddy} alt='maddy'></img>
+                <span className='wes'>esMaddy balance</span>
+                <span>{balwallet / 2 * 1}</span>
+                <button className='btn'>Claim</button>
+              </div>
+            </div>
+            {/*<span>8 pm UTC</span>*/}
             {/*<span id="demo"></span>*/}
-            <Countdown date={data.date + data.delay} onStart={(delta) => {
+            {/*<Countdown date={data.date + data.delay} onStart={(delta) => {
               if (localStorage.getItem("end_date") == null)
               localStorage.setItem(
                 "end_date",
@@ -93,14 +321,18 @@ function App() {
                 if (localStorage.getItem("end_date") != null)
                 localStorage.removeItem("end_date");
               }}
-              />
+            />*/}
+            
             {/*<MemoCountdown />*/}
+          </div>
+          <div className='wut'>
+            Total amount raised: {tvl1/1000000000000000000} ETH
           </div>
           <div className='presale-card'>
             <h1 className=''>Instructions for presale</h1>
             <p>1. Max individual purchase limit</p>
             <p>2. Presale hardcap is $25k</p>
-            <p>3. Presale contract audit link is <a href='#'>Audit</a></p>
+            <p>3. Presale contract audit link is <a href='https://contractwolf.io/projects/maddy-protocol'>Audit</a></p>
           </div>
         </div>
       </div>
